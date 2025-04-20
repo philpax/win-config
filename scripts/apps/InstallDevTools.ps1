4a01eea362bbbfc78cf0332cedca2f0695951e6d
Philpax <me@philpax.me>
2025-04-20 07:53:12 +0200

    feat: install MSVS

--- scripts/apps/InstallDevTools.ps1
+++ scripts/apps/InstallDevTools.ps1
@@ -2,6 +2,8 @@ Write-Host "Installing dev tools..." -ForegroundColor "Yellow"
 
 Import-Module (Join-Path $PSScriptRoot "..\modules\AppInstaller.psm1") -Force
 
+winget install Microsoft.VisualStudio.2022.Community --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended"
+
 installApps @(
     "Anysphere.Cursor"
     "Git.Git"
